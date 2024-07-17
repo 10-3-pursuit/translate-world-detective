41c733519d02c6d60f4b6532f99e874d9bb4ba9a
jd richards <jdrichardstech@gmail.com>
2024-07-17 17:50:51 -0400

    add fs

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 const cors = require('cors')
 require('dotenv').config()
 const { Translate } = require('@google-cloud/translate').v2
 
 const app = express()
 const apiKey = process.env.API_KEY
-// Replace with your actual API key
+console.log('google', process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64)
+
+const base64Credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64
+
+const jsonString = Buffer.from(base64Credentials, 'base64').toString('utf-8')
+
+const credentials = JSON.parse(jsonString)
+
+fs.writeFileSync('/tmp/translate.json', JSON.stringify(credentials))
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/tmp/translate.json'
 
 app.use(cors())
 app.use(express.json())
